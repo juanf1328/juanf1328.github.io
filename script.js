6ab033da10923b67f232d70ef98022bb4fc7dad9
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-09-14 00:50:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+Function sing(song){
+	console.log(song);
+	}
+	
+	sing("Laaaaa deeeee daaaaa");
+	sing("hellooooooo");
+	sing("backstreets back alright");
+	
+	Function multiply(a, b){
+	return a*b;
+	
+	}
+	
+	var total = multiply(4,5);
+	alert(total);
