07105c4b22c09b7617ae43ddb26a4bde35079438
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-09-20 15:36:31 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var user = {
+	name:"john",
+	age: 34,
+	hobby: "soccer",
+	isMarried: false,
+	spells: ["abrakadra", "shazam", "boo"],
+	shout: function(){
+		console.log("ahhhhh!");
+	}
+};
+
+
+var list = [
+{
+	username:"andy",
+	password:"secre"
+},
+{
+	username:"jess",
+	password:"123"
+}
+
+];
