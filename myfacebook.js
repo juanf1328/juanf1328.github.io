850f635285fdc95acb15ae6d46e1696b25302304
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-09-20 17:56:38 -0300

    Add files via upload

---
+++ myfacebook.js
@@ -0,0 +1,25 @@
+var database = [
+{
+	username:"juan",
+	password: "supersecreto"
+}
+];
+
+var newsfeed = [
+{
+	username:"bobby",
+	timeline:"so tired from all that learning"
+
+},
+{
+	username:"sally",
+	timeline:"Javascript is soooo coooool!"
+
+}
+];
+var usernamepromp=promt("what is your username?");
+var passwordprompt=promt("What is your password?");
+
+function signin(user, pass){
+	if (user === database[0].username)
+}
