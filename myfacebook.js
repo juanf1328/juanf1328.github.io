5620b3ff9601b6b7167e6ab4e01e01e7da04f0bf
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-09-21 17:21:59 -0300

    Add files via upload

--- myfacebook.js
+++ myfacebook.js
@@ -3,6 +3,14 @@ var database = [
 	username:"juan",
 	password: "supersecreto"
 }
+{
+	username:"sally",
+	password: "superrr"
+}
+{
+	username:"ingrid",
+	password: "1328"
+}
 ];
 
 var newsfeed = [
@@ -17,15 +25,27 @@ var newsfeed = [
 
 }
 ];
+function isuservalid(username, password){
+		for (var i=0; i < database.length; i++){
+		if(database[i].username=== username && database[i].password === password){
+			return true;
+		
+	}
+	return false;
+}
+
+function signin(user, pass){
+
+	for (var i=0; i < database.length; i++){
+		if(isuservalid(username, password)){
+			console.log(newsfeed);
+		}else{
+			alert("sorry, wrong username and password");
+		}
+	}
 var usernamepromp=promt("what is your username?");
 var passwordprompt=promt("What is your password?");
 
-function signin(user, pass){
-	if (user === database[0].username && pass === database[0].password){
-		console.log(newsfeed);
-	}else{
-		alert("sorry, wrong username and password");
-	}
 }
 
 signin(usernamepromp, passwordprompt);
