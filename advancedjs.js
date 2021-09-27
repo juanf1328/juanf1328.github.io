0e9e2b569e291d2f17338fc5ae26036c5bbe9832
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-09-27 15:32:16 -0300

    Add files via upload

---
+++ advancedjs.js
@@ -0,0 +1,26 @@
+var fun = 5;
+
+function funFunction(){
+	var fun = "helloooo";
+	console.log(1, fun);
+}
+
+function funerFunction(){
+
+	var fun = "byeee";
+	console.log(2, fun);
+}
+
+function funestFunction(){
+	fun = "AHHHH";
+	console.log(3, fun);
+}
+
+console.log("window", fun);
+funFunction();
+funerFunction();
+funestFunction();
+
+
+		
+	
