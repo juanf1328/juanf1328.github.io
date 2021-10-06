5b2ebf82946ef1761ba7fee589632966acdb9907
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-10-06 17:44:05 -0300

    Add files via upload

---
+++ ES7(ES2016).js
@@ -0,0 +1,13 @@
+.includes()
+
+/// 'Helloooo', includes('o'); deberia dar true
+
+
+const pets = ['cat', 'dog', 'bat'];
+
+pets.includes('dog')
+
+///deberia dar true 
+
+const square = (x) => x**2
+const cube = (y) => y**3
---
+++ typecoercion.js
@@ -0,0 +1,34 @@
+// Ejemplo de typecoercion: 1 === '1
+
+
+if (true){
+	console.log(5)
+}
+
+if (1){
+	console.log(5)
+}
+
+///////////////////
+
+if (false){
+	console.log(5)
+}
+
+
+if (0){
+	console.log(5)
+}
+
+
+//// Vi una tabla de comparacion de "JS Comparison Table", los casos de === o de  ==
+
+// Vi una tabla de las comparaciones (De x, y, ==, ===, Object.is, sameValue) de una pagina "MDMA"
+
+
+Object.is(-0,+0)
+
+
+NaN === NaN 
+
+// y un ejemplo de un bug de netflix que muestra como en donde deberia mostrar los minutos de la pelicula muestra 0:0NaN
