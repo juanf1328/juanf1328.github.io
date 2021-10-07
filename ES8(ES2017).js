1c9d7eb19a51c388fa65767e42146609b94006df
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-10-07 17:26:44 -0300

    Add files via upload

---
+++ ES8(ES2017).js
@@ -0,0 +1,43 @@
+.padStart()
+.padEnd()
+
+const fun = (
+	a,
+	b,
+	c,
+	d,
+	e,
+	) => {
+	console.log(a);
+}
+
+fun(1,2,3,4)
+
+Object.values
+Object.entries
+Object.keys
+
+let obj = {
+	username: 'santa',
+	username1: 'rudolf',
+	username2: 'mr.Grinch'
+
+}
+
+Object.keys(obj).forEach((key, index) =>{
+	console.log(key, obj[key]);
+})
+
+Object.values(obj).forEach(value =>{
+	console.log(value);
+})
+
+Object.entries(obj).forEach(value =>{
+	console.log(value);
+})
+
+Object.entries(obj).map(value=> {
+	return value[1] + value[0].replace('username', '');
+})
+
+Async Await
